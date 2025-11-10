fdb16dbc5b7b354967dff65f2189774e45941605
David Rivas <155179534+DvdRivas@users.noreply.github.com>
2025-11-09 19:28:25 -0700

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,3 +6,18 @@ curl -sSfL https://raw.githubusercontent.com/Mystenlabs/suiup/main/install.sh | 
 
 echo "📦 Instalando sui..."
 suiup install sui -y
+
+echo "--- Iniciando configuración interactiva de SUI Client ---"
+
+# Este bloque (<< EOF ... EOF) alimenta las siguientes líneas
+# al comando 'sui client' una por una.
+sui client << EOF
+y
+
+0
+EOF
+
+sui client new-env --alias mainnet --rpc https://fullnode.mainnet.sui.io:443
+
+echo "--- Configuración de SUI Client completada ---"
+
